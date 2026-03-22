5f238c384ceac5518f614594113615c8c618286f
RodoPAG <ra.pagani.sanchez@gmail.com>
2026-03-22 20:43:56 -0300

    add math operations to basics.py

--- basics.py
+++ basics.py
@@ -33,3 +33,14 @@ print (f"My name is {name} and I am {age} years old.")
 print (f"I am {height} tall and a {is_developer} developer.")
 
 print (f"Hello {name}")
+
+# Math operations 
+a = 10
+b = 3
+print (F"Addition: {a + b}")
+print (f"Subtraction: {a - b}")
+print (f"Multiplication: {a * b}")
+print (f"Division: {a / b}")
+print (f"Floor Division: {a // b}")
+print (f"Remainder: {a % b}")
+print (f"Power: {a ** b}")
